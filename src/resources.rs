5a02eb19b71d15ce22d9a6d83472f95803272575
Underslash12 <stew_smith@hotmail.com>
2025-03-16 23:49:52 -0700

    Changed get path

--- src/resources.rs
+++ src/resources.rs
@@ -40,7 +40,7 @@ pub async fn load_string(file_name: &str) -> anyhow::Result<String> {
     // }
 
     let path = std::path::Path::new(".\\")
-        .join("resources")
+        .join("assets")
         .join(file_name);
     // println!("{:?}", path);
     let txt = std::fs::read_to_string(path)?;
@@ -68,7 +68,7 @@ pub async fn load_binary(file_name: &str) -> anyhow::Result<Vec<u8>> {
     // }
 
     let path = std::path::Path::new(".\\")
-        .join("resources")
+        .join("assets")
         .join(file_name);
     // println!("{:?}", path);
     let data = std::fs::read(path)?;
